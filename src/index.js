6b2623fa1bfcc963f49e6ce34fb33fdc7fbf400b
CasillasCesar <78441226+CasillasCesar@users.noreply.github.com>
2021-02-05 10:11:16 -0600

    Configuración básica para el uso de documentos tipo .EJS, Creación de la página principal,Cambio de ubicación de la carpeta views

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const app  = express();
+const path = require('path');
+//Settings
+    //Creación de la variable "puerto" con su valor
+app.set('puerto', 80);
+    //selección de motor de plantilla ejs
+app.set('view engine','ejs');
+    //Configuración de ubicación de la carpeta views
+app.set('views',path.join(__dirname,'views'));
+
+//Midlewares
+
+//Routes
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+//Static files
+
+//Servidor Escucha
+app.listen(app.get('puerto'),()=>{
+    console.log('Bienvenido');
+});
