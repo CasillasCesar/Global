0449f1004c873c0b72d08e1ddff9d77341a5f38e
CasillasCesar <78441226+CasillasCesar@users.noreply.github.com>
2021-02-09 11:27:53 -0600

    Integración de partials, fuente de google y fontawesome al proyecto

--- src/routes/ruta.js
+++ src/routes/ruta.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const ruta = express.Router();
 ruta.get('/',(req,res)=>{
-    res.render('inicio.html',{mensaje: 'Este mensaje'});
+    res.render('inicio.html',{titulo: 'Inicio'});
+});
+ruta.get('/Home',(req,res)=>{
+    res.render('home.html',{titulo: 'Home'});
 });
 module.exports = ruta;
